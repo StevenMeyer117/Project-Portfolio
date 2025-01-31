45f075c5c7c07bba5e3668e8624c156fa7c47668
Steven Meyer <steven.a.meyer23@outlook.com>
2025-01-30 20:03:39 -0500

    Commit finalized program

--- HackingTheFender.py
+++ HackingTheFender.py
@@ -9,10 +9,10 @@
 # Use your knowledge of working with Python files to retrieve, manipulate, obscure, and create data in your quest for
 # justice. Work with CSV files and other text files in this exploration of the strength of Python file programming.
 
-# 14:05
 
-# Import the CSV module
+# Import the CSV and JSON modules
 import csv
+import json
 
 # Create list of users who's passwords have been compromised
 compromised_users = []
@@ -30,9 +30,29 @@ with open('compromised_users.txt', 'w') as compromised_user_file:
         compromised_user_file.write(compromised_user)
 
 
-# Import JSON module
-import json
-
+# Notify boss with encoded message in JSON
 with open('boss_message.json', 'w') as boss_message:
     boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
     json.dump(boss_message_dict, boss_message)
+
+# Remove passwords.csv by creating new passwords file with Slash Null's signature
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+    slash_null_sig = """ 
+ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+"""
+    new_passwords_obj.write(slash_null_sig)
