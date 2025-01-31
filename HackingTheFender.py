4b444cc04f3f2d4c8af5ce98901d8b801a817565
Steven Meyer <steven.a.meyer23@outlook.com>
2025-01-30 19:37:20 -0500

    Commit reading in passwords and notifying the boss

--- HackingTheFender.py
+++ HackingTheFender.py
@@ -9,7 +9,30 @@
 # Use your knowledge of working with Python files to retrieve, manipulate, obscure, and create data in your quest for
 # justice. Work with CSV files and other text files in this exploration of the strength of Python file programming.
 
-
+# 14:05
 
 # Import the CSV module
 import csv
+
+# Create list of users who's passwords have been compromised
+compromised_users = []
+
+# Open 'passwords' CSV file. Pass 'password_file' object holder to CSV reader for parsing. Save each row to temp var
+# and add each username to compromised users list
+with open('passwords.csv') as password_file:
+    password_csv = csv.DictReader(password_file)
+    for password_row in password_csv:
+        compromised_users.append(password_row['Username'])
+
+# Open file 'compromised_users.txt' in write-mode and write username of each user in new file
+with open('compromised_users.txt', 'w') as compromised_user_file:
+    for compromised_user in compromised_users:
+        compromised_user_file.write(compromised_user)
+
+
+# Import JSON module
+import json
+
+with open('boss_message.json', 'w') as boss_message:
+    boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
+    json.dump(boss_message_dict, boss_message)
